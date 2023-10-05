ebce3931c99d04bfb39c845c56b3f10d87a16e7f
Muzamil Zaman Khan <95095602+MuzamilDev@users.noreply.github.com>
2023-10-05 19:42:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var weblinkone = document.getElementById("learning-web-o");
+var weblinktwo = document.getElementById("learning-web-t");
+var weblinkthree = document.getElementById("learning-web-h");
+var weblinkfour = document.getElementById("learning-web-f");
+
+
+weblinkone.addEventListener("mouseover",function(){
+    window.open("https://kanaeokana.net/olelo");
+});
+
+weblinktwo.addEventListener("mouseover",function(){
+    window.open("https://www.gohawaii.com/hawaiian-culture/hawaiian-language-guide");
+});
+
+weblinkthree.addEventListener("mouseover",function(){
+    window.open("https://www.nps.gov/havo/learn/historyculture/olelo-hawaii.htm");
+});
+
+weblinkfour.addEventListener("mouseover",function(){
+    window.open("https://www.hawaiitourismauthority.org/");
+});
